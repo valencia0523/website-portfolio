320a1dcacaaa20ea36aada9f56a5f62963f58995
Valencia Miyeon Jang <miyeon5230@gmail.com>
2025-03-19 11:54:15 +0900

    projects detail

--- components/layouts/CardContainer.tsx
+++ components/layouts/CardContainer.tsx
@@ -4,47 +4,22 @@ import { fetchProjects } from '@/lib/contentful';
 import { useEffect, useRef, useState } from 'react';
 import { Project } from '@/lib/types';
 import CardItem from '../ui/CardItem';
-import gsap from 'gsap';
 
 const CardContainer = () => {
   const [projects, setProjects] = useState<Project[]>([]);
   const containerRef = useRef<HTMLDivElement>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const getProjects = async () => {
       const data = await fetchProjects();
       setProjects(data);
-      setIsLoaded(true);
     };
     getProjects();
   }, []);
 
-  useEffect(() => {
-    if (projects.length > 0 && containerRef.current) {
-      gsap.fromTo(
-        containerRef.current.children,
-        { opacity: 0, x: -50 },
-        {
-          opacity: 1,
-          x: 0,
-          duration: 3,
-          stagger: 0.2,
-          ease: 'power3.out',
-          clearProps: 'all',
-        }
-      );
-    }
-  }, [projects]);
-
   return (
     <div className="bg-yellow-50">
-      <div
-        ref={containerRef}
-        className={`max-w-6xl mx-auto py-12 px-8 grid grid-cols-1 md:grid-cols-3 gap-6 ${
-          isLoaded ? '' : 'opacity-0'
-        }`}
-      >
+      <div className="max-w-6xl mx-auto py-12 px-8 grid grid-cols-1 md:grid-cols-3 gap-6">
         {projects.map((item) => {
           return <CardItem key={item.id} {...item} />;
         })}
--- components/ui/CardItem.tsx
+++ components/ui/CardItem.tsx
@@ -1,34 +1,98 @@
 import { Project } from '@/lib/types';
 import Image from 'next/image';
 import ImageComingSoon from '@/public/images/image-coming-soon.jpg';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogTitle,
+  DialogHeader,
+  DialogDescription,
+} from '@/components/ui/dialog';
 
 const CardItem = (props: Project) => {
-  const { id, title, url, techStack, image, description } = props;
+  const { id, title, url, githubUrl, techStack, image, description } = props;
   const imageSrc = image ? `https:${image}` : ImageComingSoon;
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // CardItem animation effect
+  useEffect(() => {
+    gsap.fromTo(
+      containerRef.current,
+      { opacity: 0, x: -50 },
+      { opacity: 1, x: 0, duration: 2, ease: 'power3.out' }
+    );
+  }, []);
 
   return (
-    <div
-      className="flex flex-col items-center shadow-xl bg-white rounded-[12px]
-        hover:shadow-2xl hover:cursor-pointer"
-    >
-      <div className="rounded-[12px] m-3">
-        <div className="w-[280px] h-[170px] mt-2 lg:w-[320px] lg:h-[200px]">
-          <Image
-            src={imageSrc}
-            alt={title}
-            width={400}
-            height={300}
-            className="rounded-[10px] object-cover w-full h-full overflow-hidden"
-          />
-        </div>
-        <div className="m-2 w-[265px] lg:m-4">
-          <div className="text-xl lg:text-2xl">{title}</div>
-          <div className="text-gray-600 mt-1 line-clamp-3 h-[70px] overflow-hidden">
-            {description}
+    <Dialog>
+      <DialogTrigger>
+        <div
+          className="flex flex-col items-center shadow-xl bg-white rounded-[12px]
+        hover:shadow-2xl hover:cursor-pointer opacity-0"
+          ref={containerRef}
+        >
+          <div className="rounded-[12px] m-3">
+            <div className="w-[280px] h-[170px] mt-2 lg:w-[320px] lg:h-[200px]">
+              <Image
+                src={imageSrc}
+                alt={title}
+                width={400}
+                height={300}
+                className="rounded-[10px] object-cover w-full h-full overflow-hidden"
+              />
+            </div>
+            <div className="m-2 w-[265px] lg:m-4">
+              <div className="text-xl lg:text-2xl">{title}</div>
+              <div className="text-gray-600 mt-1 line-clamp-3 h-[70px] overflow-hidden">
+                {description}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </DialogTrigger>
+
+      <DialogContent className="flex flex-col items-center rounded-[12px]">
+        <DialogHeader>
+          <div className="mt-3.5 px-1.5 w-[320px] h-[200px] rounded-[12px]">
+            <Image
+              src={imageSrc}
+              alt={title}
+              width={500}
+              height={400}
+              className="w-full h-full object-fill rounded-[12px]"
+            />
+          </div>
+          <DialogTitle className="text-xl font-semibold text-left ml-3 capitalize">
+            {title}
+          </DialogTitle>
+          <div className="flex flex-row gap-3 justify-start ml-3">
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-[#FFE87C] px-1 py-0.5 rounded-[4px]"
+            >
+              Live Site
+            </a>
+            <a
+              href={githubUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-[#FFE87C] px-1 py-0.5 rounded-[4px]"
+            >
+              GitHub
+            </a>
+          </div>
+        </DialogHeader>
+        <DialogDescription>
+          <p className="text-black">{description}</p>
+          <div className="text-gray-600 mt-1">{techStack}</div>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   );
 };
 
