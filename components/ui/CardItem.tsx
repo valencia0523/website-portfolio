07e1fdb4881bf15219e570341284340b11a79b6d
Valencia Miyeon Jang <miyeon5230@gmail.com>
2025-03-20 00:54:36 +0900

    Finished projects page

--- components/ui/CardItem.tsx
+++ components/ui/CardItem.tsx
@@ -31,11 +31,11 @@ const CardItem = (props: Project) => {
       <DialogTrigger>
         <div
           className="flex flex-col items-center shadow-xl bg-white rounded-[12px]
-        hover:shadow-2xl hover:cursor-pointer opacity-0"
+      hover:shadow-2xl hover:cursor-pointer opacity-0 w-full max-w-[320px]"
           ref={containerRef}
         >
-          <div className="rounded-[12px] m-3">
-            <div className="w-[280px] h-[170px] mt-2 lg:w-[320px] lg:h-[200px]">
+          <div className="rounded-[12px] py-3 lg:px-3">
+            <div className="w-full max-w-[280px] aspect-[16/9] mt-2 lg:max-w-[300px]">
               <Image
                 src={imageSrc}
                 alt={title}
@@ -44,9 +44,9 @@ const CardItem = (props: Project) => {
                 className="rounded-[10px] object-cover w-full h-full overflow-hidden"
               />
             </div>
-            <div className="m-2 w-[265px] lg:m-4">
+            <div className="text-left my-2 pl-1.5 w-full max-w-[265px] lg:mx-1 lg:my-4">
               <div className="text-xl lg:text-2xl">{title}</div>
-              <div className="text-gray-600 mt-1 line-clamp-3 h-[70px] overflow-hidden">
+              <div className="text-gray-600 mt-1 line-clamp-3 h-[70px] overflow-hidden lg:mt-2">
                 {description}
               </div>
             </div>
@@ -54,26 +54,27 @@ const CardItem = (props: Project) => {
         </div>
       </DialogTrigger>
 
-      <DialogContent className="flex flex-col items-center rounded-[12px]">
+      <DialogContent className="flex flex-col items-center rounded-[12px] px-3 lg:max-w-[40%]">
         <DialogHeader>
-          <div className="mt-3.5 px-1.5 w-[320px] h-[200px] rounded-[12px]">
+          <div className="mt-3 w-full max-w-[600px] aspect-[16/9] rounded-[12px] lg:max-w-[700px]">
             <Image
               src={imageSrc}
               alt={title}
-              width={500}
+              width={800}
               height={400}
               className="w-full h-full object-fill rounded-[12px]"
             />
           </div>
-          <DialogTitle className="text-xl font-semibold text-left ml-3 capitalize">
+          <DialogTitle className="text-xl font-semibold text-left ml-1.5 capitalize lg:text-4xl lg:mt-4">
             {title}
           </DialogTitle>
-          <div className="flex flex-row gap-3 justify-start ml-3">
+          <div className="flex flex-row gap-3 justify-start ml-1.5 lg:text-xl lg:mt-0.5">
             <a
               href={url}
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-[#FFE87C] px-1 py-0.5 rounded-[4px]"
+              className="bg-[#FFE87C] px-1 py-0.5 rounded-[4px] 
+              hover:bg-yellow-300 hover:shadow-md"
             >
               Live Site
             </a>
@@ -81,15 +82,16 @@ const CardItem = (props: Project) => {
               href={githubUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-[#FFE87C] px-1 py-0.5 rounded-[4px]"
+              className="bg-[#FFE87C] px-1 py-0.5 rounded-[4px]
+               hover:bg-yellow-300 hover:shadow-md"
             >
               GitHub
             </a>
           </div>
         </DialogHeader>
-        <DialogDescription>
+        <DialogDescription className="mx-1.5 lg:text-[1.2rem] lg:mx-5 lg:my-3">
           <p className="text-black">{description}</p>
-          <div className="text-gray-600 mt-1">{techStack}</div>
+          <div className="text-gray-600 mt-1.5 lg:mt-2">{techStack}</div>
         </DialogDescription>
       </DialogContent>
     </Dialog>
